3ad16550cf5402356c5fab5ae29d94282c3f377e
Nguyen Van Hung <nguyenvanhung.13t4@gmail.com>
2018-11-28 20:37:28 +0700

    layout cho paging

--- src/app/quan-ly-tai-san/hien-thi-tai-san/hien-thi-tai-san.component.ts
+++ src/app/quan-ly-tai-san/hien-thi-tai-san/hien-thi-tai-san.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TaiSan} from '../../_models/tai-san';
 import {TaiSanService} from '../../_services/tai-san.service';
-import {PageService} from '../../_services/page.service';
 
 @Component({
   selector: 'app-hien-thi-tai-san',
@@ -11,14 +10,8 @@ import {PageService} from '../../_services/page.service';
 export class HienThiTaiSanComponent implements OnInit {
 
   taiSans: TaiSan[];
-    pager: any = {};
 
-    // paged items
-    pagedItems: any[];
-    itemPerPage: any = 5;
-
-  constructor(private taiSanService: TaiSanService,
-              private pageService: PageService) {
+  constructor(private taiSanService: TaiSanService) {
     this.taiSans = [];
   }
 
@@ -26,23 +19,9 @@ export class HienThiTaiSanComponent implements OnInit {
     this.taiSanService.getAll().subscribe(
         result => {
           this.taiSans = result;
-            this.setPage(1);
         }, error2 => {
           alert('Lỗi');
         }
     )
   }
-
-    setPage(page: number) {
-        // get pager object from service
-        this.pager = this.pageService.getPager(this.taiSans.length, page, this.itemPerPage);
-
-        // get current page of items
-        this.pagedItems = this.taiSans.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    }
-
-    setItemPerPage(value: any) {
-      this.itemPerPage = value;
-      this.setPage(1);
-    }
 }
