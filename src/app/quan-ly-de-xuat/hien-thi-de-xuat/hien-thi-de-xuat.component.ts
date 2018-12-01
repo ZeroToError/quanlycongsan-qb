199af900e2a2967fb2313b5fc51ab460b549e67c
Minh Hoang <minhhoang97hk@gmail.com>
2018-12-01 20:18:34 +0700

    giao dien phan de xuat

--- src/app/quan-ly-de-xuat/hien-thi-de-xuat/hien-thi-de-xuat.component.ts
+++ src/app/quan-ly-de-xuat/hien-thi-de-xuat/hien-thi-de-xuat.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HienThiDeXuatComponent implements OnInit {
 
-  constructor() { }
+    total: number;
+    page: number;
+    size: number ;
+    totalPage: number;
+  constructor() {
+      this.total = 0;
+      this.page = 0;
+      this.size = 10;
+      this.totalPage = 0;
+  }
 
   ngOnInit() {
   }
 
+    setItemPerPage(itemPerPage: number) {
+        this.size = itemPerPage;
+        this.page = 0;
+    }
+    arrayOne(): any[] {
+        return Array(this.totalPage);
+    }
+
+    changePage(page: number) {
+        this.page = page - 1;
+        this.getTaiSans();
+    }
+
+    previousPage() {
+        this.page--;
+        this.getTaiSans();
+    }
+    nextPage() {
+        this.page++;
+        this.getTaiSans();
+    }
 }
